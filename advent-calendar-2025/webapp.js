0c37c5a45733950889b359a7eeab8b5523e0be1f
GilletPaul <pgillet.iphone@gmail.com>
2025-11-21 21:51:00 +0100

    2025

---
+++ advent-calendar-2025/webapp.js
@@ -0,0 +1,9 @@
+// function loadWebApp() {
+//     const input = document.getElementById("webapp-url");
+//     const iframe = document.getElementById("webapp-iframe");
+
+//     iframe.src = "https://drive.google.com/file/d/10k6Wz1vlLIaNcm7IjvDxEyhJeNyTR0dm/preview";
+// }
+
+// // Optionnel : ouvrir l’app proprement dans ton OS
+
